7e4a60b3019bdfe31abc95e3c6782e02b693bcf3
Sungho <hurgj123kr@gmail.com>
2023-05-30 15:51:35 +0900

    Declarative programming use

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,26 +1,25 @@
-import { useApolloClient, gql } from "@apollo/client"
+import { gql, useQuery } from "@apollo/client"
 import { useEffect, useState } from "react"
 
 function Home() {
-  const [movies, setMoives] = useState([])
-  const client = useApolloClient()
-  useEffect(() => {
-    client
-      .query({
-        query: gql`
-          {
-            allMovies {
-              id
-              title
-            }
-          }
-        `,
-      })
-      .then((result) => setMoives(result.data.allMovies))
-  }, [client])
+  const movies = gql`
+    query getMovies {
+      allMovies {
+        id
+        title
+      }
+    }
+  `
+  const { data, loading, error } = useQuery(movies)
+  if (loading) {
+    return <strong>Loading..,</strong>
+  }
+  if (error) {
+    return <h1>Could not fetch</h1>
+  }
   return (
     <ul>
-      {movies.map((movie) => (
+      {data.allMovies.map((movie) => (
         <li key={movie.id}>{movie.title}</li>
       ))}
     </ul>
