1b58643e38fb0759fc4a83fe91df009dfecffc4f
Sungho <hurgj123kr@gmail.com>
2023-05-31 13:31:49 +0900

    get id > want movie get

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,5 @@
 import { gql, useQuery } from "@apollo/client"
-import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 
 function Home() {
   const movies = gql`
@@ -20,7 +20,9 @@ function Home() {
   return (
     <ul>
       {data.allMovies.map((movie) => (
-        <li key={movie.id}>{movie.title}</li>
+        <li key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
       ))}
     </ul>
   )
--- src/routes/Movie.js
+++ src/routes/Movie.js
@@ -1,5 +1,25 @@
+import { gql, useQuery } from "@apollo/client"
+import { useParams } from "react-router-dom"
+
+const getMovie = gql`
+  query getMovie($movieId: ID!) {
+    movie(id: $movieId) {
+      id
+      title
+    }
+  }
+`
 function Movie() {
-  return <h1>Movie</h1>
+  const { id } = useParams()
+  const { data, loading } = useQuery(getMovie, {
+    variables: {
+      movieId: id,
+    },
+  })
+  if (loading) {
+    return <h1>Looking for movie...</h1>
+  }
+  return <h1>{data.movie.title}</h1>
 }
 
 export default Movie
