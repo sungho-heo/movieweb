125585bd7ba703ede678cb964f4682ded9b9b974
Sungho <hurgj123kr@gmail.com>
2023-10-16 21:13:36 +0900

    gihub page deploy4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./routes/Home.js";
 import Movie from "./routes/Movie.js";
 import home from "./img/home.png";
