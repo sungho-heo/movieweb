82cd64e17fefb636e09b60e754fdae78f81053e1
Sungho <hurgj123kr@gmail.com>
2023-10-16 20:59:47 +0900

    gihub page deploy3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./routes/Home.js";
 import Movie from "./routes/Movie.js";
 import home from "./img/home.png";
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className={"header"}>
         <Link to={"/"}>
           <img className={"logo"} alt="home" src={home} />
