b6fcfaf197d3ec000e6bfba2a4a5cdbf2003e84c
Sungho <hurgj123kr@gmail.com>
2023-10-20 15:24:46 +0900

    uri upadet

--- src/apllo.js
+++ src/apllo.js
@@ -1,7 +1,7 @@
 import { InMemoryCache, ApolloClient } from "@apollo/client";
 
 const apllo = new ApolloClient({
-  uri: "http://0.0.0.0:4000/",
+  uri: "http://graphql-api-study-dev.ap-northeast-2.elasticbeanstalk.com",
   cache: new InMemoryCache(),
 });
 export default apllo;
