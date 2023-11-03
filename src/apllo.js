fef7802a897aefdc5a250f497334461da92ccac3
Sungho <hurgj123kr@gmail.com>
2023-11-03 14:26:13 +0900

    uri last update

--- src/apllo.js
+++ src/apllo.js
@@ -1,7 +1,7 @@
 import { InMemoryCache, ApolloClient } from "@apollo/client";
 
 const apllo = new ApolloClient({
-  uri: "http://graphql-api-study-dev.ap-northeast-2.elasticbeanstalk.com",
+  uri: "https://www.movieweb.shop/",
   cache: new InMemoryCache(),
 });
 export default apllo;
